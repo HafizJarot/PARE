f1bfafa56f24a64ce003b117ae5d3774557553ca
HafizJarot <hafizalexandria@gmail.com>
2020-04-16 22:00:01 +0700

    procfile

--- app/Http/Controllers/v1/PenyewaReklame/Auth/LoginController.php
+++ app/Http/Controllers/v1/PenyewaReklame/Auth/LoginController.php
@@ -28,19 +28,12 @@ class LoginController extends Controller
 
         if (Auth::guard('web')->attempt($credentials)){
             $user = Auth::guard('web')->user();
-            if ($user-> active == '2'){
                 return response()->json([
                     'status' => true,
                     'message' => 'Anda berhasil login',
                     'data' => $user,
                 ], 200);
-            }else{
-                return response()->json([
-                    'status' => false,
-                    'massage' => 'Silahkan menunggu konfirmasi dari kami',
-                    'data' => (object)[],
-                ], 200);
-            }
+
         }else{
             return response()->json([
                 'status' => false,
